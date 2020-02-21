59645d305f8ddf2b2be86387f110a635b8a989a2
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-02-21 17:34:15 +0100

    Modificaciones de funciones.

--- Funciones.py
+++ Funciones.py
@@ -1,21 +1,28 @@
 #Ejercicio 1.
 
 def ListarCoches(doc):
-    modelos=doc.xpath("//modelo//text()")
-    marcas=doc.xpath("//marca//text()")
+    modelos=doc.xpath("//modelo/text()")
+    marcas=doc.xpath("//marca/text()")
+    potencias=doc.xpath("//potencia/text()")
+    puertas=doc.xpath("//puertas/text()")
+    precios=doc.xpath("//precio/text()")
+    co2s=doc.xpath("//co2/text()")
+    consumos=doc.xpath("//consumo/text()")
+    velocidades=doc.xpath("//velmax/text()")
     coches=[]
     for elem,elem1 in zip(modelos,marcas):
         coches.append(elem+elem1)
-    cochesnorepetidos=[]
-    for i in coches:
-        if i not in cochesnorepetidos:
-            cochesnorepetidos.append(i)
-    return cochesnorepetidos
+    return coches, potencias, puertas, precios, co2s, consumos, velocidades
 
 #Ejercicio 2.
 
 def ContarConcesionarios(doc):
-    return len(doc.xpath("/concesionarios[count(coches)>3]"))
+    concesionarios=doc.xpath("/concesionarios")
+    concesionariosmastres=[]
+    for i in concesionarios:
+        if len(i.xpath("./coches"))>3:
+            concesionariosmastres.append(i.xpath("./nombre/text()"))
+    return concesionariosmastres
 
 #Ejercicio 3.
 
@@ -25,3 +32,10 @@ def CochesPorConcesionario(doc,concesionario):
     coches=[]
     for elem,elem1 in zip(modelos,marcas):
         coches.append(elem+elem1)
+    return coches
+
+#Funciones extra.
+
+def ValidarConcesionario(doc,concesionario):
+    if concesionario in doc.xpath("/concesionarios/concesionario/nombre/text()"):
+        return True
